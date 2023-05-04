1a748981b9569ab2f87f1e64c06aba8bb21adb90
jin <53258214+kauthenticity@users.noreply.github.com>
2023-05-04 16:44:57 +0900

    💬 소개 문구 수정

--- src/components/Introduce.tsx
+++ src/components/Introduce.tsx
@@ -5,16 +5,17 @@ export const Introduce = () => {
   return (
     <section className="container">
       <h1>
-        깔끔함을 추구하는
+        질문을 원동력으로 성장하는
         <br />
         프론트엔드 개발자 강진실입니다.
       </h1>
       <Description>
-        미디어, 패션과 같은 라이프스타일에 관심이 많습니다.
+        기술이 바꾸는 라이프스타일에 관심이 많습니다.
+        
+        <br />
+        누구나 쉽게 읽을 수 있는 코드를 작성하고자 합니다.
         <br />
         UI 디자인 및 구현 경험을 통해 사용자 인터렉션에 대한 이해도가 높습니다.
-        <br />
-        누구나 쉽게 읽을 수 있을 수 있는 코드를 작성하고자 노력합니다.
       </Description>
       <TableWithBorder
         tableElements={[
