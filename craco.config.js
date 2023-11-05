8d5d9d9211b30a7da03d005178963f88ca2ba2d7
jin <53258214+kauthenticity@users.noreply.github.com>
2023-11-05 21:32:30 +0900

    👷 craco 및 절대 경로 설정

---
+++ craco.config.js
@@ -0,0 +1,11 @@
+const path = require("path");
+
+module.exports = {
+  webpack: {
+    alias: {
+      "@components": path.resolve(__dirname, "src/components"),
+      "@lib": path.resolve(__dirname, "src/lib"),
+      "@constants": path.resolve(__dirname, "src/constants"),
+    },
+  },
+};
