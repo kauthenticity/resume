837bbf985a653a382892304615366cdfd68df888
jin <53258214+kauthenticity@users.noreply.github.com>
2023-11-05 20:11:02 +0900

    🚚 type파일에 .type 추가

--- src/constants/sideProjects.type.ts
+++ src/constants/sideProjects.type.ts
@@ -5,6 +5,7 @@ export const SIDE_PROJECTS_DATA: TSideProject[] = [
     imagePath: "/images/healthier.png",
     title: "헬시어",
     description: "증상 감별 및 건강 관리 서비스",
+    url: "",
     techStacks: [
       {
         text: "React",
@@ -28,6 +29,7 @@ export const SIDE_PROJECTS_DATA: TSideProject[] = [
     imagePath: "/images/hannip.png",
     title: "한입",
     description: "스타의 팬들을 위한 굿즈 나눔 플랫폼",
+    url: "https://www.notion.so/kauthenticity/74ccaf1f81f64fe194edad325ef3910e?pvs=4",
     techStacks: [
       {
         text: "React-Native",
@@ -44,6 +46,30 @@ export const SIDE_PROJECTS_DATA: TSideProject[] = [
       {
         text: "React-Query",
         className: "react__query",
+      },
+    ],
+  },
+  {
+    imagePath: "/images/bobta.png",
+    title: "밥타",
+    description: "AI 기반 식사 모임 매칭 서비스",
+    url: "https://kauthenticity.notion.site/906deec27e424c1bb4e6d51b212602e0?pvs=4",
+    techStacks: [
+      {
+        text: "React",
+        className: "react",
+      },
+      {
+        text: "Typescript",
+        className: "typescript",
+      },
+      {
+        text: "React-Query",
+        className: "react__query",
+      },
+      {
+        text: "Styled-Component",
+        className: "styled__components",
       },
     ],
   },
