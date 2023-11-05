d97f697d48c26d8f89d36ed87acf446b2c8070b1
jin <53258214+kauthenticity@users.noreply.github.com>
2023-11-05 23:05:40 +0900

    🔨 types, styles 절대 경로 설정

--- craco.config.js
+++ craco.config.js
@@ -6,6 +6,8 @@ module.exports = {
       "@components": path.resolve(__dirname, "src/components"),
       "@lib": path.resolve(__dirname, "src/lib"),
       "@constants": path.resolve(__dirname, "src/constants"),
+      "@types": path.resolve(__dirname, "src/types"),
+      "@styles": path.resolve(__dirname, "src/styles"),
     },
   },
 };
