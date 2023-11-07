f95739c5963a7d2dd270aa6fa64d63a97cd3b452
jin <53258214+kauthenticity@users.noreply.github.com>
2023-11-07 19:10:38 +0900

    🎨 Introduce 스타일 파일 분리

---
+++ src/components/Introduce/index.style.tsx
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+
+export const Description = styled.div`
+    color: var(--gray700);
+    font-size: 1.125rem;
+    line-height: 160%;
+`;
+
+export const ContactButtonList = styled.ul`
+    display: flex;
+    align-items: center;
+    gap: 12px;
+
+    margin-top: 16px;
+    margin-bottom: 0;
+`;
+
+export const TitleSpan = styled.span`
+    font-size: 0.95em;
+`;
+
+export const Image = styled.img`
+    width: 120px;
+    height: 120px;
+
+    border-radius: 4px;
+`;
